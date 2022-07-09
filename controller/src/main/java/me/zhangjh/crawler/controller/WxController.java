1869a9336cc19692f6e79522a9b8bb8816a2802f
zhangjh <njhxzhangjihong@126.com>
2022-07-10 00:16:20 +0800

    wx check token

--- controller/src/main/java/me/zhangjh/crawler/controller/WxController.java
+++ controller/src/main/java/me/zhangjh/crawler/controller/WxController.java
@@ -1,13 +1,12 @@
 package me.zhangjh.crawler.controller;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Collections;
@@ -23,7 +22,7 @@ public class WxController {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private static final String TOKEN = "8FqhwyzIjsmNx2HAfusQrei3p7gSe7XeBEetzPSlCcV";
+    private static final String TOKEN = "wiredSheep";
 
     private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'a', 'b', 'c', 'd', 'e', 'f' };
@@ -35,17 +34,7 @@ public class WxController {
         List<String> list = Arrays.asList(TOKEN, timestamp, nonce);
         Collections.sort(list);
         String join = String.join("", list);
-        MessageDigest sha1 = MessageDigest.getInstance("SHA1");
-        sha1.update(join.getBytes(StandardCharsets.UTF_8));
-        byte[] digest = sha1.digest();
-        int j = digest.length;
-        char[] buf = new char[j * 2];
-        int k = 0;
-        for (byte byte0 : digest) {
-            buf[k++] = HEX_DIGITS[byte0 >>> 4 & 0xf];
-            buf[k++] = HEX_DIGITS[byte0 & 0xf];
-        }
-        String sha1Str = new String(buf);
+        String sha1Str = DigestUtils.sha1Hex(join);
         log.info("sha1Str: {}", sha1Str);
         boolean checkRet = sha1Str.equals(signature);
         log.info("checkSignature ret: {}", checkRet);
