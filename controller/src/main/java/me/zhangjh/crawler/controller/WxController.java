013e70d51fbc9115bace5461991847fb968741cb
zhangjh <njhxzhangjihong@126.com>
2022-07-10 00:20:00 +0800

    wx check token

--- controller/src/main/java/me/zhangjh/crawler/controller/WxController.java
+++ controller/src/main/java/me/zhangjh/crawler/controller/WxController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -24,21 +23,20 @@ public class WxController {
 
     private static final String TOKEN = "wiredSheep";
 
-    private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'a', 'b', 'c', 'd', 'e', 'f' };
-
     @RequestMapping("/msg")
     @ResponseBody
-    public boolean checkSignature(String signature, String timestamp, String nonce) throws NoSuchAlgorithmException {
+    public String checkSignature(String signature, String timestamp, String nonce, String echostr) {
         log.info("checkSignature, signature: {}, timestamp: {}, nonce: {}", signature, timestamp, nonce);
         List<String> list = Arrays.asList(TOKEN, timestamp, nonce);
         Collections.sort(list);
         String join = String.join("", list);
         String sha1Str = DigestUtils.sha1Hex(join);
         log.info("sha1Str: {}", sha1Str);
-        boolean checkRet = sha1Str.equals(signature);
-        log.info("checkSignature ret: {}", checkRet);
-        return checkRet;
+        if(sha1Str.equals(signature)) {
+            return echostr;
+        } else {
+            return "";
+        }
     }
 
 }
